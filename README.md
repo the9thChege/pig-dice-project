61bea02b5560e1595f65fed772e06846292f9de9
Chege <chegekformal@gmail.com>
2017-05-12 15:42:32 +0300

    First Upload. The 2 Player Mode is fully functional

---
+++ README.md
@@ -0,0 +1,34 @@
+## AUTHOR
+Chege Kariuki
+## PROJECT NAME
+Pig Dice
+## DESCRIPTION
+A dice game.
+## INSTALLATION
+Available on GitHub.
+## USAGE
+A 2 player game. Click "Start" to initiate.
+## HISTORY
+Began on 12/5/17.
+## CREDITS
+Ohinton & StackOverFlow.com
+## SPECS
+The program makes use of a Player constructor.
+
+The Player constructor has the following properties:
+    1. Player.roll
+    2. Player.tempscore
+    3. Player.totalscore
+    4. Player.turn
+
+There are also the following prototype functions:
+    1. rollone.
+    2. hold.
+    3. winnerCheck.
+    4. newGame.
+
+++The program has a one-player mode against a computer opponent.
+
+++The one-player mode can be set on two difficulties.
+## License
+None.
---
+++ js/scripts.js
@@ -0,0 +1,100 @@
+//Back-end Logic
+
+var throwdice = function() {
+  return Math.floor(6 * Math.random()) + 1;
+};
+
+function Player(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+}
+
+Player.prototype.rollone = function() {
+  if (this.roll === 1) {
+    this.tempscore = 0;
+    alert("Sorry Player 1, you rolled a 1! Your turn is over!");
+    // this.changeturn();
+  } else {
+    this.tempscore += this.roll;
+  }
+}; //if a 1 is rolled then turn ends
+
+Player.prototype.hold = function() {
+  this.totalscore += this.tempscore;
+  this.tempscore = 0;
+  // this.changeturn();
+  alert("Pass the mouse!");
+}; //Hold
+
+Player.prototype.winnerCheck = function() {
+  if (this.totalscore >= 100) {
+    alert("You are the winner!");
+  }
+};
+
+Player.prototype.newGame = function() {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+}; //Starts a New Game
+
+////////////////////////////////////////////////////////////////////////
+
+// UI Logic
+$(document).ready(function() {
+
+  $("button#start").click(function(event) {
+    player1 = new Player(true);
+    player2 = new Player(false);
+    $(".playfield").show();
+    $(".start").hide();
+  });
+
+  $("button#new-game").click(function(event) {
+    $(".playfield").hide();
+    player1.newGame();
+    player2.newGame();
+    $("#player1Current").empty();
+    $("#player1Score").empty();
+    $("#player1Streak").empty();
+    $("#player2Current").empty();
+    $("#player2Score").empty();
+    $("#player2Streak").empty();
+
+    $(".start").show();
+  });
+
+  $("button#player1Button").click(function(event) {
+    player1.roll = throwdice();
+    $("#player2Streak").empty();
+    $("#player1Streak").text(player1.roll);
+    player1.rollone();
+    $("#player1Current").text(player1.tempscore);
+  });
+
+  $("button#player2Button").click(function(event) {
+    player2.roll = throwdice();
+    $("#player1Streak").empty();
+    $("#player2Streak").text(player2.roll);
+    player2.rollone();
+    $("#player2Current").text(player2.tempscore);
+  });
+
+  $("button#player1Pass").click(function(event) {
+    player1.hold();
+    $("#player1Score").text(player1.totalscore);
+//    $("#player1Current").empty();
+    $("#player1Streak").empty();
+    player1.winnerCheck();
+  });
+
+  $("button#player2Pass").click(function(event) {
+    player2.hold();
+    $("#player2Score").text(player2.totalscore);
+//    $("#player2Current").empty();
+    $("#player2Streak").empty();
+    player2.winnerCheck();
+  });
+});
